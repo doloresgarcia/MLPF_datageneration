24c0bd0c708f7aa9a22741fc7ca5a972294fbbe9
Bohdan Dudar <bohdan96@gmail.com>
2025-09-06 17:11:36 +0200

    Add stable check for performance...

--- scripts/count_sim_decays.cpp
+++ scripts/count_sim_decays.cpp
@@ -123,6 +123,8 @@ void count_sim_decays() {
 
         decayHappenedFlags = std::vector<bool>(decayNames.size()); // reset every event
         for(const auto& mc : mcs){
+            auto mcStable = mc.generatorStatus() == 1 || mc.generatorStatus() == 0;
+            if (not mcStable) continue;
 
             for(int j=0; j < decayNames.size(); ++j){
                 auto isDecay = decayFuncs[j];
